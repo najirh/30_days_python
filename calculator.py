053699fe89acbab532f77d87c85946c6cded00de
Jean NZONZIDI <jeannzonzidi@gmail.com>
2024-10-01 16:15:30 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -27,6 +27,8 @@ def mul(num1, num2):
 
 # division
 def div(num1, num2):
+    if num2 == 0:
+        return "Error! Division by zero."
     return num1 / num2
 
 def calculator(num1, num2, operation):
